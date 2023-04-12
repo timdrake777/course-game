10e22fa95a4ba3929cf839a43ef8f1a12e207427
Muslimov_DR <muslimov@muslimov>
2023-04-12 14:34:09 +0400

    Добавлены счетчик монет и задний фон

--- Client/src/components/GameView/GameView.tsx
+++ Client/src/components/GameView/GameView.tsx
@@ -1,12 +1,9 @@
 import Konva from "konva";
 import { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { Layer, Rect, Sprite, Stage } from "react-konva";
+import { useNavigate } from "react-router";
 import useImage from "use-image";
-import changePosition from "../../utils/gameViewActions/changePosition";
-import { AnimationType, CharacterConfig, CHARACTER_SPRITE_ANIMATIONS } from "../../utils/constants";
 
-import BgImagePNG from "../../assets/Background/Floor.png";
-import characterPNG from "../../assets/Character/character.png";
 import {
   IAnimationType,
   ILevelConfig,
@@ -15,13 +12,21 @@ import {
   IPositionResponse,
 } from "../../interfaces";
 import storageActions from "../../utils/storageActions";
-import obstacleSprites from "../../utils/gameViewActions/obstacleSprites";
-import GameButton from "../Templates/GameButton";
-import { useNavigate } from "react-router";
 import coinAnimation from "../../utils/gameViewActions/coinAnimation";
-import EmptyConfig from "./EmptyConfig";
-import coinSound from "../../assets/Audio/coin.mp3";
-import deathSound from "../../assets/Audio/death.mp3";
+import obstacleSprites from "../../utils/gameViewActions/obstacleSprites";
+import changePosition from "../../utils/gameViewActions/changePosition";
+import { AnimationType, CharacterConfig, CHARACTER_SPRITE_ANIMATIONS } from "../../utils/constants";
+import GameButton from "../Templates/GameButton";
+import EmptyConfig from "./fragments/EmptyConfig";
+
+import coinSfx from "../../assets/Audio/coin.mp3";
+import deathSfx from "../../assets/Audio/death.mp3";
+import levelCompleteSfx from "../../assets/Audio/levelComplete.wav";
+import BgImagePNG from "../../assets/Background/Floor.png";
+import characterPNG from "../../assets/Character/character.png";
+
+import styles from "./GameView.module.scss"
+import CoinsCounter from "./fragments/CoinsCounter";
 
 export const GameView = () => {
   const navigate = useNavigate();
@@ -30,20 +35,20 @@ export const GameView = () => {
   const characterRef = useRef<Konva.Sprite>(null);
   const keyUpRef = useRef<boolean>(false);
   const keyDownTimeoutRef = useRef<number>();
-  const coinsCount = useRef<number>(0);
 
   const [posX, setPosX] = useState<number>(CharacterConfig.START_POSITION_X);
   const [posY, setPosY] = useState<number>(CharacterConfig.START_POSITION_Y);
   const [animationType, setAnimationType] = useState<IAnimationType>(AnimationType.IDLE);
-  const [levelConfig, setLevelConfig] = useState<ILevelConfig | null>(
-    storageActions.getLevelConfig()
-  );
+  const [coinsCount, setCoinsCount] = useState<number>(0);
+
+  const levelConfig = useMemo<ILevelConfig | null>(() => storageActions.getLevelConfig(), []);
   const obstacles: IObstacles = useMemo(() => obstacleSprites(levelConfig), [levelConfig]);
 
   const [bgImage] = useImage(BgImagePNG);
   const [characterImage] = useImage(characterPNG);
-  const [coinGetSound] = useState(new Audio(coinSound));
-  const [deadSound] = useState(new Audio(deathSound));
+  const coinGetSound = useMemo(() => new Audio(coinSfx), [coinSfx]);
+  const deathSound = useMemo(() => new Audio(deathSfx), [deathSfx]);
+  const levelCompleteSound = useMemo(() => new Audio(levelCompleteSfx), [levelCompleteSfx]);
 
   const arrowTimeout = () =>
     setTimeout(() => {
@@ -103,20 +108,31 @@ export const GameView = () => {
         case 1:
           setTimeout(() => setAnimationType(AnimationType.DEAD), 100);
           removeKeyEvents();
-          deadSound.play();
+          deathSound.play();
           clearTimeout(keyDownTimeoutRef.current);
           setTimeout(() => characterRef.current?.stop(), 670);
           break;
         case 2:
+          let currentCoinsCount = coinsCount;
+          currentCoinsCount += 1;
           let sprite = viewRef.current?.findOne(`#${response.id}`) as Konva.Sprite;
 
           if (!sprite) break;
 
+          if (currentCoinsCount === obstacles.coins) {
+            removeKeyEvents();
+            clearTimeout(keyDownTimeoutRef.current);
+            coinGetSound.addEventListener("ended", () => {
+              levelCompleteSound.play();
+              setAnimationType(AnimationType.IDLE);
+            });
+          }
+
           coinGetSound.pause();
           coinGetSound.currentTime = 0;
           coinGetSound.play();
-          coinsCount.current += 1;
           coinAnimation(sprite, response);
+          setCoinsCount(currentCoinsCount)
           break;
         default:
           break;
@@ -151,16 +167,17 @@ export const GameView = () => {
   }, [characterImage]);
 
   return (
-    <section className="w-1/2 h-full flex items-center justify-center">
+    <section className={"w-1/2 h-full flex items-center justify-center relative " + styles.gameViewSection} >
       {levelConfig ? (
         <>
-          <GameButton
+          <CoinsCounter currentCoinsCount={coinsCount} allCoins={obstacles.coins}/>
+          {/* <GameButton
             onClick={(e) => navigate("config")}
             func="ok"
             className="absolute top-0 left-0 text-slate-700"
           >
             Конфигурация
-          </GameButton>
+          </GameButton> */}
           <Stage height={levelConfig.height()} width={levelConfig.width()} ref={viewRef}>
             <Layer>
               <Rect
