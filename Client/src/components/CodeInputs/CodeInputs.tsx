9ad5bbfef20d36dbdae406cdeb3e7614436056c0
DRMus <78694252+DRMus@users.noreply.github.com>
2023-05-03 01:30:27 +0400

    Реализован интерфейс и добавление инпутов

--- Client/src/components/CodeInputs/CodeInputs.tsx
+++ Client/src/components/CodeInputs/CodeInputs.tsx
@@ -1,35 +1,67 @@
 import { FormEvent, useRef, useState } from "react";
 
-import styles from './CodeInputs.module.scss'
+import styles from "./CodeInputs.module.scss";
+import { ControlViewInput } from "../ControlView/ControlViewInput";
 
 export const CodeInputs = () => {
-    const inputRef = useRef(null);
-    const [inputs, setInputs] = useState<string[]>([""]);
+  const [inputs, setInputs] = useState<string[]>([""]);
+  const [focusedInput, setFocusedInput] = useState<number>(0);
 
-    const validateInput = (e: FormEvent<HTMLDivElement>, index: number) => {
-        const lastChar = e.currentTarget.textContent?.charAt(e.currentTarget.textContent.length - 1)
-        const oldInputIndex = inputs[index];
-        console.log(lastChar)
-        if (lastChar && /[a-zA-Z]/.test(lastChar)) {
-            console.log(lastChar)
-            setInputs((prev) => {
-                prev[index] = e.currentTarget.textContent || "";
-                return prev;
-            })
-        } else {
-            setInputs((prev) => {
-                prev[index] = oldInputIndex;
-                return prev;
-            })
-        }
+  const validateInput = (e: FormEvent<HTMLDivElement>, index: number) => {
+    const lastChar = e.currentTarget.textContent?.charAt(e.currentTarget.textContent.length - 1);
+    const oldInputIndex = inputs[index];
+    console.log(lastChar);
+    if (lastChar && /[a-zA-Z]/.test(lastChar)) {
+      console.log(lastChar);
+      setInputs((prev) => {
+        prev[index] = e.currentTarget.textContent || "";
+        return prev;
+      });
+    } else {
+      setInputs((prev) => {
+        prev[index] = oldInputIndex;
+        return prev;
+      });
     }
+  };
 
-    return (
-        <>
-            {inputs.map((item, index) => (
-                <div key={index} suppressContentEditableWarning={true} contentEditable className={styles.inputCode} onInput={(e) => validateInput(e, index)} ref={inputRef}>
-                    {item}
-                </div>))}
-        </>
-    )
-}
+  const onInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, idx: number) => {
+    const { key } = e;
+
+    switch (key) {
+      case "Enter":
+        e.preventDefault();
+        setInputs((prev) => {
+          prev.splice(idx + 1, 0, "");
+          console.log(prev);
+          return prev;
+        });
+        setFocusedInput(focusedInput + 1);
+        break;
+    }
+  };
+
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>, idx: number) => {
+    setInputs((prev) => {
+      prev[idx] = e.target.value;
+      return prev;
+    });
+  };
+
+  const changeFocus = (index: number) => setFocusedInput(index);
+
+  return (
+    <>
+      {inputs.map((item, index) => (
+        <ControlViewInput
+          key={index}
+          index={index}
+          focusedInput={focusedInput}
+          changeFocus={changeFocus}
+          onKeyDown={onInputKeyDown}
+          onChange={changeInput}
+        />
+      ))}
+    </>
+  );
+};
--- Client/src/components/ControlView/ControlViewInput.tsx
+++ Client/src/components/ControlView/ControlViewInput.tsx
@@ -1,15 +1,41 @@
+import { FC, useEffect, useLayoutEffect, useRef } from "react";
+import styles from "./ControlViewInput.module.scss";
 
 interface InputProps {
-    index: number,
-    
+  index: number;
+  focusedInput: number;
+  changeFocus: (index: number) => void;
+  onKeyDown: (e: React.KeyboardEvent<HTMLInputElement>, idx: number) => void;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>, idx: number) => void;
 }
 
-export const ControlViewInput = () => {
-    return (
-        <div className="inputBox">
-            <div className="inputId"></div>
-            <input type="text" className="input" />
-            <div className="deleteBtn"></div>
-        </div>
-    )
-}
+export const ControlViewInput: FC<InputProps> = (props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current && props.focusedInput === props.index) {
+      inputRef.current.focus();
+    } else if (inputRef.current) {
+      inputRef.current.blur();
+    }
+  }, [props.focusedInput, inputRef.current]);
+
+  return (
+    <div className={styles.inputBox} onClick={() => inputRef.current?.focus()}>
+      <input
+        type="text"
+        className="peer px-1 bg-transparent focus:bg-[#4d4d4d] order-2"
+        onKeyDown={(e) => props.onKeyDown(e, props.index)}
+        onFocus={() => props.changeFocus(props.index)}
+        onChange={(e) => props.onChange(e, props.index)}
+        ref={inputRef}
+      />
+      <div className="peer-focus:bg-[#666] flex justify-end items-center px-1 order-1 text-white/70">
+        {props.index + 1}
+      </div>
+      <div className="hidden peer-focus:bg-[#4d4d4d] peer-focus:flex order-3 items-center">
+        <button className={styles.deleteBtn}>X</button>
+      </div>
+    </div>
+  );
+};
