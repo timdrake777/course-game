9919988e7861d94dd99da940945928f5d80dd8c7
dd.bondarenko <>
2023-05-03 12:54:14 +0400

    Небольшая правка выпиливания ссылки

--- Client/src/components/GameView/fragments/GameViewSettings.tsx
+++ Client/src/components/GameView/fragments/GameViewSettings.tsx
@@ -1,16 +1,17 @@
 import { faGear } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const GameViewSettings = () => {
   return (
-    <a
-      href="/config"
+    <Link
+      to="/config"
       className="text-zinc-200 cursor-pointer hover:text-zinc-400 transition-colors"
       title="Конфигурация"
     >
       <FontAwesomeIcon icon={faGear} size="lg" />
-    </a>
+    </Link>
   );
 };
 
